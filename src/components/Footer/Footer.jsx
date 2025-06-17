e62a3a6d957142cc7c6abe0794b78d4f88d9d823
Carlosmagno23 <deltamagno01@gmail.com>
2025-06-17 15:37:15 -0300

    Segundo commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,14 +10,25 @@ import XIcon from '../../assets/x-icon.svg'
 import LinkedinIcon from '../../assets/linkedin-icon.svg'
 import InstagramIcon from '../../assets/instagram-icon.svg'
 
+// CONTEXT
+import { AppContext } from '../../contexts/AppContext'
+import { useContext } from 'react'
+import Button from '../Button/Button'
+
+
 function Footer () {
+    const appContext = useContext(AppContext)
+    const changeLanguage = (country) => {
+        appContext.setLanguage(country)
+    }
+
     return (
         <footer>
             <div className="container">
                 <div className="d-flex jc-space-between mobile-fd-column">
                     <div className="footer-logo-col">
                         <img src={Logo} className='footer-logo'/>
-                        <p className='grey-1-color'>A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
+                        <p className='grey-1-color'>{appContext.languages[appContext.language].general.footerLogoText}</p>
                         <div className='d-flex social-links'>
                             <a href='https://google.com' target='_blank'>
                                 <img src={FacebookIcon} />
@@ -35,16 +46,16 @@ function Footer () {
                     </div>
                     <div className='d-flex mobile-fd-column'>
                         <div className='footer-col'>
-                            <h3>Pages</h3>
+                            <h3>{appContext.languages[appContext.language].general.pages}</h3>
                             <ul>
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to="/about">About</Link></li>
-                                <li><Link to="/projects">Projects</Link></li>
-                                <li><Link to="/contact">Contact</Link></li>
+                                <li><Link to="/">{appContext.languages[appContext.language].menu.home}</Link></li>
+                                <li><Link to="/about">{appContext.languages[appContext.language].menu.about}</Link></li>
+                                <li><Link to="/projects">{appContext.languages[appContext.language].menu.projects}</Link></li>
+                                <li><Link to="/contact">{appContext.languages[appContext.language].menu.contact}</Link></li>
                             </ul>
                         </div>
                         <div className='footer-col'>
-                            <h3>Contacts</h3>
+                            <h3>{appContext.languages[appContext.language].menu.contact}</h3>
                             <p className="grey-1-color">R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030</p>
                             <p className="grey-1-color">suporte@escoladnc.com.br</p>
                             <p className="grey-1-color">(19) 99187-4342</p>
@@ -54,8 +65,12 @@ function Footer () {
                 <div className='d-flex jc-space-between footer-copy'>
                     <p className='grey-1-color'>Copyright © DNC - 2024</p>
                     <div className='langs-area d-flex'>
-                        <img src={BrazilLogo} height="29px"/>
-                        <img src={UsaLogo} height="29px"/>
+                        <Button buttonStyle="image-button" onClick={() => changeLanguage('br')}>
+                            <img src={BrazilLogo} height="29px"/>
+                        </Button>
+                        <Button buttonStyle="image-button" onClick={() => changeLanguage('en')}>
+                            <img src={UsaLogo} height="29px"/>
+                        </Button>   
                     </div>
                 </div>
             </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,17 +2,20 @@ import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 import Banner from "../components/Banner/Banner"
 import ProjectsList from "../components/ProjectsList/ProjectsList"
+import { useContext } from 'react'
+
+// CONTEXT
+import { AppContext } from '../contexts/AppContext'
 
 function Projects() {
+    const appContext = useContext(AppContext)
     return (
         <>
             <Header />
-            <Banner title="Projects" image="projects.jpg" />
+            <Banner title={appContext.languages[appContext.language].menu.projects} image="projects.jpg" />
             <div className="container">
-                
                 <ProjectsList />
             </div>
-            
             <Footer />
         </>
     )
